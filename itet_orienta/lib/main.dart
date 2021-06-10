fa4e788e9aa4cccb4e58ea55cc78f5f706188fce
Enrico La Sala <enrico.lasala@buildnn.com>
2021-06-10 10:37:54 +0200

    Aggiornamento struttura codice

--- itet_orienta/lib/main.dart
+++ itet_orienta/lib/main.dart
@@ -3,18 +3,28 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final appTitle = 'ITET Orienta';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ITET Orienta',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('ITET Orienta'),
-        ),
-        body: const Center(
-          child: const Text('Applicazione Orientamento - ITET Garibaldi Marsala'),
-        ),
+      title: appTitle,
+      theme: ThemeData(
+        primaryColor: Colors.purple,
       ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final title = 'ITET Orienta';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: Center(child: Text('Applicazione Orientamento - ITET "Garibaldi" Marsala')),
     );
   }
 }
