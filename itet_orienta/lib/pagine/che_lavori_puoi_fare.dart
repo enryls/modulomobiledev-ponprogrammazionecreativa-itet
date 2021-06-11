a2a1fd081b9f3e70bbe06bc28b25ce9b5a4c523c
Giosuè <giosue02spadafora@gmail.com>
2021-06-11 12:02:06 +0200

    Aggiornamento Pag. Lavori_puoi_fare

--- itet_orienta/lib/pagine/che_lavori_puoi_fare.dart
+++ itet_orienta/lib/pagine/che_lavori_puoi_fare.dart
@@ -4,15 +4,29 @@ class CheLavoriPuoiFare extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Che lavori puoi fare?'),
-      ),
-      body: ListView(
-        children: [
-          Image.asset('assets/images/scuola.png', width: 600, height: 240, fit: BoxFit.cover,),
-          Text('Testo della pagina 5'),
-        ],
-      ),
+        appBar: AppBar(
+          title: Text("Add Padding to Text Widget in Flutter"),
+        ),
+        body: Column(
+            children: <Widget>[
+              Image.asset('assets/images/scuola.png',
+                  width: 600,
+                  height: 240,
+                  fit: BoxFit.cover
+              ),
+
+              Center(child:Container(
+                  padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                  child: Text("Itet  Garibaldi offre degli sbocchi lavorativi ai ragazzi interessati  a entrare in contatto con il mondo dell'economia, in particolare svolgendo attività lavorative di marketing, contabili e finanziarie.",
+                      style: TextStyle(fontSize: 22)))),
+
+              Padding(
+                  padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                  child: Text('Con il diploma presso l’Itet si può lavorare nelle banche, nelle assicurazioni, nelle imprese, uffici pubblici, libero professionista, programmatore/informatico o  negli enti pubblici accessibili tramite concorso. Ma non solo, si può diventare direttore in strutture alberghiere, villaggi turistici, e lavorare in agenzie di viaggi, uffici della pubblica amministrazione, compagnie di aeroporti e imprese di trasporto.',
+                      style: TextStyle(fontSize: 22))),
+
+            ]
+        )
     );
   }
 }
