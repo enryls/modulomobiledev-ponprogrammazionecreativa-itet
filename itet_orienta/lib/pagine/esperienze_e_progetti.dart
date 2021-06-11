10c73a9e521992c79356c72972d1f9049cd8fb66
chiaraocc <occhipinti.chiara@itetgaribaldi.it>
2021-06-11 12:01:49 +0200

    modifiche alla pagina (esperienze e progetti)

--- itet_orienta/lib/pagine/esperienze_e_progetti.dart
+++ itet_orienta/lib/pagine/esperienze_e_progetti.dart
@@ -5,14 +5,25 @@ class EsperienzeEProgetti extends StatelessWidget {
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: Text('Esperienze e progetti'),
+        title: Text(
+            'Esperienze e Progetti',
+             style: TextStyle(
+            color: Colors.amber
+        )
+          ),
       ),
       body: ListView(
         children: [
-          Image.asset('assets/images/scuola.png', width: 600, height: 240, fit: BoxFit.cover,),
-          Text('Testo della pagina 3'),
-        ],
-      ),
+              Image.asset('assets/images/experience.jpeg', width: 600, height: 240, fit: BoxFit.cover,),
+              Text(
+                'Il nostro Istituto presenta diverse attività ed esperienze innovative che prospettano il miglioramento del nostro modo di pensare,non solo nello studio ma perfino nel lavoro. Esse sono attività che suscitano molto interesse,fatti in modo tale da poter aiutare gli alunni a crescere e ad avere nuove conoscenze.',
+                  style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'Times New Roman'
+                ),
+              )
+             ],
+           ),
     );
   }
 }
