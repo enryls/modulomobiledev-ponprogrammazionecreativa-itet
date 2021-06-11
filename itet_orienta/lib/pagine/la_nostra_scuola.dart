ed79c3ef2a0808881d142a5b45091ab8a094d531
giovannuzzo <ingoglia.giovanni@itetgaribaldi.it>
2021-06-11 11:58:46 +0200

    modifiche alla pagina (nostra scuola)

--- itet_orienta/lib/pagine/la_nostra_scuola.dart
+++ itet_orienta/lib/pagine/la_nostra_scuola.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class LaNostraScuola extends StatelessWidget {
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -9,8 +10,11 @@ class LaNostraScuola extends StatelessWidget {
       ),
       body: ListView(
         children: [
-          Image.asset('assets/images/scuola.png', width: 600, height: 240, fit: BoxFit.cover,),
-          Text('Testo della pagina 1'),
+          Image.asset('assets/images/scuola_2.png', width: 600, height: 200, fit: BoxFit.cover,),
+          Text(''),
+          Text('La Nostra Scuola',textAlign: TextAlign.center,style: TextStyle(fontSize: 24,)),
+          Text(''),
+          Text('La nostra storia inizia nel 1921 con l’istituto superiore per il commercio… Nel 1961 la scuola viene intitolata a Giuseppe Garibaldi. Nel corso degli anni seguenti l’istituto si è sviluppato sempre di più. Tra gli anni 80 e gli anni 90 il “Giuseppe Garibaldi” arrivò ad avere fino a 45 classi e più di 1300 alunni, continuando ad evolversi diventando, oggi, una delle scuole più rinomate e punto di riferimento per l’istruzione superiore nel territorio.'),
         ],
       ),
     );
