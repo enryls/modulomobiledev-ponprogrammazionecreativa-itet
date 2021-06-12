82cd49ebc97583c2896aab4aaf38413805e4bb05
Enrico La Sala <enrico.lasala@buildnn.com>
2021-06-12 02:43:35 +0200

    Aggiunta pagina contatti

--- itet_orienta/lib/pagine/contatti.dart
+++ itet_orienta/lib/pagine/contatti.dart
@@ -1,1 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:itet_orienta/drawer.dart';
+
+class Contatti extends StatelessWidget {
+  final title = 'ITET Orienta';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title:
+      Image.asset('assets/images/itetorienta.png',fit: BoxFit.cover,),
+          centerTitle: true
+      ),
+      body: ListView(
+        children: [
+          Image.asset('assets/images/scuola.png', width: 600, height: 240, fit: BoxFit.cover,),
+          Text('Contatti'),
+        ],
+      ),
+      drawer: MyDrawer(),
+    );
+  }
+}
