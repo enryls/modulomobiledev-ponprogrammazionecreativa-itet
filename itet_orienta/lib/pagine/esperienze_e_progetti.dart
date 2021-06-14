0d8cebea51a013143454697c0d86c7e835a89c4f
chiaraocc <occhipinti.chiara@itetgaribaldi.it>
2021-06-14 16:44:20 +0200

    modifiche alla pagina esperienze e progetti

--- itet_orienta/lib/pagine/esperienze_e_progetti.dart
+++ itet_orienta/lib/pagine/esperienze_e_progetti.dart
@@ -14,15 +14,63 @@ class EsperienzeEProgetti extends StatelessWidget {
         children: [
           Image.asset('assets/images/experience.jpeg', width: 600, height: 240, fit: BoxFit.cover,),
           SizedBox(height: 10),
-          Text('Esperienze e progetti',textAlign: TextAlign.center ,style: TextStyle(fontSize: 35, color: Colors.amber)),
+          Text('Esperienze e Progetti',textAlign: TextAlign.center ,style: TextStyle(fontSize: 35, color: Colors.amber, fontStyle: FontStyle.italic)),
           SizedBox(height: 10),
           Text(
             'Il nostro Istituto presenta diverse attività ed esperienze innovative '
                 'che prospettano il miglioramento del nostro modo di pensare, '
                 'non solo nello studio ma perfino nel lavoro. Esse sono attività '
                 'che suscitano molto interesse,fatti in modo tale da poter aiutare '
-                'gli alunni a crescere e ad avere nuove conoscenze.',
+                'gli alunni a crescere e ad avere nuove conoscenze. Alcune di queste sono:',
             style: TextStyle(
+                fontSize: 20,
+                fontFamily: 'Times New Roman'
+            ),
+          ),
+          SizedBox(height: 10),
+          Text('\u27A9 ERASMUS PLUS ',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              fontFamily: 'Times New Roman',
+          ),
+          ),
+          SizedBox(height: 10),
+      Text('\u27A9 INTERCULTURA ',
+        style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            fontFamily: 'Times New Roman'
+            ),
+           ),
+          SizedBox(height: 10),
+          Text('\u27A9 CERTIFICAZIONE ECDL ',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                fontFamily: 'Times New Roman'
+            ),
+          ),
+          SizedBox(height: 10),
+          Text('\u27A9 PON ',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                fontFamily: 'Times New Roman'
+            ),
+          ),
+          SizedBox(height: 10),
+          Text('\u27A9 TRINITY ',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                fontFamily: 'Times New Roman'
+            ),
+          ),
+          SizedBox(height: 10),
+          Text('\u27A9 STAGE ',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
                 fontSize: 20,
                 fontFamily: 'Times New Roman'
             ),
