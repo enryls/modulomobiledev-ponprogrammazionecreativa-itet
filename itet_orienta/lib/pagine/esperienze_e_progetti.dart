37df2b378e5bc62a26dbb60987cef4bcee6cac7f
chiaraocc <occhipinti.chiara@itetgaribaldi.it>
2021-06-15 12:05:53 +0200

    cambi alla pagina esperienze e progetti

--- itet_orienta/lib/pagine/esperienze_e_progetti.dart
+++ itet_orienta/lib/pagine/esperienze_e_progetti.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:itet_orienta/drawer.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class EsperienzeEProgetti extends StatelessWidget {
   @override
@@ -23,58 +24,79 @@ class EsperienzeEProgetti extends StatelessWidget {
                 'che suscitano molto interesse,fatti in modo tale da poter aiutare '
                 'gli alunni a crescere e ad avere nuove conoscenze. Alcune di queste sono:',
             style: TextStyle(
-                fontSize: 20,
+                fontSize: 22,
                 fontFamily: 'Times New Roman'
             ),
           ),
           SizedBox(height: 10),
-          Text('\u27A9 ERASMUS PLUS ',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              fontFamily: 'Times New Roman',
+          Row(children: <Widget>[
+            Text('\u27A9 ', style:TextStyle(color:Color.fromRGBO(93, 0, 255, 1.0), fontSize: 30)),
+            Text('ERASMUS PLUS ', style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Times New Roman', fontSize: 22)
+               ),
+            TextButton(
+              style: TextButton.styleFrom(
+                padding: const EdgeInsets.all(16.0),
+                primary: Colors.black,
+                textStyle: const TextStyle(fontSize: 20),
+              ),
+              child: const Text('clicca qui'),
+              onPressed:() {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => PaginaImmagine())
+                     );
+                   },
+                 ),
+             ],
           ),
-          ),
-          SizedBox(height: 10),
-      Text('\u27A9 INTERCULTURA ',
-        style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
-            fontFamily: 'Times New Roman'
-            ),
-           ),
-          SizedBox(height: 10),
-          Text('\u27A9 CERTIFICAZIONE ECDL ',
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                fontFamily: 'Times New Roman'
-            ),
-          ),
-          SizedBox(height: 10),
-          Text('\u27A9 PON ',
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                fontFamily: 'Times New Roman'
-            ),
+            SizedBox(height: 10),
+          Row(children: <Widget>[
+            Text('\u27A9 ', style:TextStyle(color:Color.fromRGBO(93, 0, 255, 1.0), fontSize: 30)),
+            Text('INTERCULTURA ', style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Times New Roman', fontSize: 22))
+          ],
           ),
           SizedBox(height: 10),
-          Text('\u27A9 TRINITY ',
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                fontFamily: 'Times New Roman'
-            ),
+          Row(children: <Widget>[
+            Text('\u27A9 ', style:TextStyle(color:Color.fromRGBO(93, 0, 255, 1.0), fontSize: 30)),
+            Text('CERTIFICAZIONE ECDL ', style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Times New Roman', fontSize: 22))
+          ],
           ),
           SizedBox(height: 10),
-          Text('\u27A9 STAGE ',
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                fontFamily: 'Times New Roman'
-            ),
+          Row(children: <Widget>[
+            Text('\u27A9 ', style:TextStyle(color:Color.fromRGBO(93, 0, 255, 1.0), fontSize: 30)),
+            Text('PON ', style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Times New Roman', fontSize: 22))
+          ],
           ),
+          SizedBox(height: 10),
+          Row(children: <Widget>[
+            Text('\u27A9 ', style:TextStyle(color:Color.fromRGBO(93, 0, 255, 1.0), fontSize: 30)),
+            Text('TRINITY', style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Times New Roman', fontSize: 22))
+          ],
+          ),
+          SizedBox(height: 10),
+          Row(children: <Widget>[
+                Text('\u27A9 ', style:TextStyle(color:Color.fromRGBO(93, 0, 255, 1.0), fontSize: 30)),
+                Text('STAGE', style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Times New Roman', fontSize: 22))
+           ],
+          ),
+        ],
+        ),
+      );
+  }
+}
+
+class PaginaImmagine extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Image.asset('assets/images/itetorienta.png', fit: BoxFit.cover,),
+          centerTitle: true
+      ),
+      body: ListView(
+        children: [
+          Image.asset('assets/images/erasmus+.jpg', fit: BoxFit.cover,)
         ],
       ),
     );
