bb8c8f5a0d985fd7d0d8db94bef8be3b43d7b12d
giovannuzzo <ingoglia.giovanni@itetgaribaldi.it>
2021-06-15 11:49:42 +0200

    cambiamento pagina contatti
    #3

--- itet_orienta/lib/pagine/contatti.dart
+++ itet_orienta/lib/pagine/contatti.dart
@@ -5,19 +5,87 @@ class Contatti extends StatelessWidget {
   final title = 'ITET Orienta';
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(title:
-      Image.asset('assets/images/itetorienta.png',fit: BoxFit.cover,),
+      drawer: MyDrawer(),
+      appBar: AppBar(
+          title: Image.asset('assets/images/itetorienta.png',fit: BoxFit.cover,),
           centerTitle: true
       ),
       body: ListView(
         children: [
-          Image.asset('assets/images/scuola.png', width: 600, height: 240, fit: BoxFit.cover,),
-          Text('Contatti'),
+          SizedBox(height: 30),
+          Text('Contatti',textAlign: TextAlign.center ,style: TextStyle(fontSize: 35,fontWeight: FontWeight.bold,)),
+          SizedBox(height: 1),
+          Padding(
+            padding: const EdgeInsets.all(20.20),
+            child:
+            Text('Per ricevere delle maggiori informazioni riguardo i servizi scolastici contattateci',
+                style: TextStyle(fontSize: 18, height: 1.2)),
+          ),
+          SizedBox(height: 1),
+          Padding(
+            padding: const EdgeInsets.all(20.20),
+            child:
+            Text('Telefono:',
+                style: TextStyle(fontSize: 18, height: 1.2)),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.20),
+          child:
+          Text('- via Trapani: 0923989011',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(20.20),
+            child:
+            Text('- via Fici: 092325098',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(20.20),
+            child:
+            Text('Fax: 0923989056',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(20.20),
+            child:
+            Text('Email: tptd03000e@istruzione.it',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(20.20),
+            child:
+            Text('Posta certificata: TPTD03000E@pec.istruzione.it',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
         ],
       ),
-      drawer: MyDrawer(),
     );
   }
 }
